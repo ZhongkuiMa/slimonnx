0e586b143b5d72859e8567874665c95afad95fb1
ZhongkuiMa <zhongkuima0419@gmail.com>
2025-04-21 09:52:32 +1000

    Add new test scripts for nn4sys and TinyImageNet models

---
+++ test_vnncomp/test_nn4sys.py
@@ -0,0 +1,28 @@
+import onnx
+
+from slimonnx import SlimONNX
+
+if __name__ == "__main__":
+    slimonnx = SlimONNX()
+    onnx_path = (
+        "../../vnncomp2024_benchmarks/benchmarks/nn4sys_2023/onnx/"
+        "mscn_2048d_dual.onnx"
+    )
+
+    # Convert the model to version 22 to avoid many inconsistencies
+    model = onnx.load(onnx_path)
+    model = onnx.version_converter.convert_version(model, target_version=22)
+    onnx_path = onnx_path.replace(".onnx", "_v22.onnx")
+    onnx.save(model, onnx_path)
+
+    target_path = onnx_path.replace(".onnx", "_simplified.onnx")
+
+    slimonnx.slim(
+        onnx_path,
+        target_path,
+        constant_to_initializer=True,
+        fuse_matmul_add=True,
+        reorder_by_strict_topological_order=True,
+        simplify_node_name=True,
+        verbose=True,
+    )
