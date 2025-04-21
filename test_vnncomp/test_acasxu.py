7b5f9a187809f89451d00b9bd0883502fba32c27
ZhongkuiMa <zhongkuima0419@gmail.com>
2025-04-21 10:07:52 +1000

    Refactor import statements in _optimize.py; Update test_acasxu.py

--- test_vnncomp/test_acasxu.py
+++ test_vnncomp/test_acasxu.py
@@ -1,14 +1,19 @@
+import onnx
+
 from slimonnx import SlimONNX
 
 if __name__ == "__main__":
     slimonnx = SlimONNX()
-    onnx_path = "../../vnncomp2024_benchmarks/benchmarks/acasxu_2023/onnx/ACASXU_run2a_1_1_batch_2000.onnx"
+    onnx_path = (
+        "../../vnncomp2024_benchmarks/benchmarks/acasxu_2023/onnx/"
+        "ACASXU_run2a_1_1_batch_2000.onnx"
+    )
 
     # Convert the model to version 22 to avoid many inconsistencies
-    # model = onnx.load(onnx_path)
-    # model = onnx.version_converter.convert_version(model, target_version=22)
-    # onnx_path = onnx_path.replace(".onnx", "_v22.onnx")
-    # onnx.save(model, onnx_path)
+    model = onnx.load(onnx_path)
+    model = onnx.version_converter.convert_version(model, target_version=22)
+    onnx_path = onnx_path.replace(".onnx", "_v22.onnx")
+    onnx.save(model, onnx_path)
 
     target_path = onnx_path.replace(".onnx", "_simplified.onnx")
 
